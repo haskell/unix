1061a6fa2ccc0038efd2caa70894087c880bb399
Ian Lynagh <igloo@earth.li>
2007-07-22 00:29:56 +0000

    Move throwErrnoPath* functions to base:Foreign.C.Error

--- System/Posix/Error.hs
+++ System/Posix/Error.hs
@@ -22,29 +22,4 @@ module System.Posix.Error (
   ) where
 
 import Foreign.C.Error
-import Foreign.Ptr
-import Foreign.Marshal.Error 	( void )
 
-throwErrnoPath :: String -> FilePath -> IO a
-throwErrnoPath loc path =
-  do
-    errno <- getErrno
-    ioError (errnoToIOError loc errno Nothing (Just path))
-
-throwErrnoPathIf :: (a -> Bool) -> String -> FilePath -> IO a -> IO a
-throwErrnoPathIf pred loc path f  = 
-  do
-    res <- f
-    if pred res then throwErrnoPath loc path else return res
-
-throwErrnoPathIf_ :: (a -> Bool) -> String -> FilePath -> IO a -> IO ()
-throwErrnoPathIf_ pred loc path f  = void $ throwErrnoPathIf pred loc path f
-
-throwErrnoPathIfNull :: String -> FilePath -> IO (Ptr a) -> IO (Ptr a)
-throwErrnoPathIfNull  = throwErrnoPathIf (== nullPtr)
-
-throwErrnoPathIfMinus1 :: Num a => String -> FilePath -> IO a -> IO a
-throwErrnoPathIfMinus1 = throwErrnoPathIf (== -1)
-
-throwErrnoPathIfMinus1_ :: Num a => String -> FilePath -> IO a -> IO ()
-throwErrnoPathIfMinus1_  = throwErrnoPathIf_ (== -1)
