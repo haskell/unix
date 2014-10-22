2eb243fa2e8e522ce88a72ad32502c933db18347
Roland Zumkeller <Roland.Zumkeller@gmail.com>
2014-10-22 08:32:39 -0400

    Modifying declaration of execvpe on OpenBSD
    This is to avoid a conflict with the declaration in OpenBSD's
    /usr/include/unistd.h, yielding "Bad header file: execvpe.h" in the
    configure phase.

--- include/execvpe.h
+++ include/execvpe.h
@@ -20,7 +20,11 @@
 
 #if !defined(_MSC_VER) && !defined(__MINGW32__) && !defined(_WIN32)
 #ifndef __QNXNTO__
+#ifdef __OpenBSD__
+extern int execvpe(const char *, char *const *, char *const *);
+#else
 extern int execvpe(char *name, char *const argv[], char **envp);
+#endif
 #endif
 extern void pPrPr_disableITimers (void);
 #endif
