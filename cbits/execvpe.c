haskell/unix

commit f24ba78f68b2cbc4f4afadc8dd60fc2935357255

Refactor local `execvpe(3)` implementation
The previous code was prone to conflicts with when the platform happens
to expose a `execvpe(3)` implementation in its libc.

This commit renames the internal implementation to `__hsunix_execvpe` as
well as adding an autoconf-detection for the presence of `execvpe(3)`,
in which case `__hsunix_execvpe()` forwards the call to `execvpe(3)`.

Moreover, the code has been cleaned up to remove likely bitrotted CPP
conditionals.

This should fix #22

(This also partially addresses #11 on platforms which have a
libc-provided `execvpe(3)`)

cbits/execvpe.c
@@ -2,19 +2,25 @@
    (c) The University of Glasgow 1995-2004
 
    Our low-level exec() variant.
+
+   Note: __hsunix_execvpe() is very similiar to the function
+         execvpe(3) as provided by glibc 2.11 and later. However, if
+         execvpe(3) is available, we use that instead.
+
    -------------------------------------------------------------------------- */
 #include "execvpe.h"
 
-#ifdef __GLASGOW_HASKELL__
-#include "Rts.h"
+#include "HsUnixConfig.h"
+
+#if HAVE_EXECVPE
+# define _GNU_SOURCE
 #endif
 
-#if !(defined(_MSC_VER) || defined(__MINGW32__) || defined(_WIN32)) /* to the end */
-#ifndef __QNXNTO__
-
-/* Evidently non-Posix. */
-/* #include "PosixSource.h" */
-
+#include <errno.h>
+#include <sys/types.h>
+#if HAVE_SYS_WAIT_H
+# include <sys/wait.h>
+#endif
 #include <unistd.h>
 #include <sys/time.h>
 #include <stdlib.h>
@@ -59,8 +65,11 @@
  */
 
 int
-execvpe(char *name, char *const argv[], char **envp)
+__hsunix_execvpe(const char *name, char *const argv[], char *const envp[])
 {
+#if HAVE_EXECVPE
+    return execvpe(name, argv, envp);
+#else
     register int lp, ln;
     register char *p;
     int eacces=0, etxtbsy=0;
@@ -75,18 +84,18 @@ execvpe(char *name, char *const argv[], char **envp)
 
     /* Get the path we're searching. */
     if (!(path = getenv("PATH"))) {
-#ifdef HAVE_CONFSTR
+# ifdef HAVE_CONFSTR
         ln = confstr(_CS_PATH, NULL, 0);
         if ((cur = path = malloc(ln + 1)) != NULL) {
 	    path[0] = ':';
 	    (void) confstr (_CS_PATH, path + 1, ln);
 	}
-#else
+# else
         if ((cur = path = malloc(1 + 1)) != NULL) {
 	    path[0] = ':';
 	    path[1] = '\0';
 	}
-#endif
+# endif
     } else
 	cur = path = strdup(path);
 
@@ -157,16 +166,5 @@ execvpe(char *name, char *const argv[], char **envp)
     if (buf)
 	free(buf);
     return (-1);
-}
-#endif
-
-
-/* Copied verbatim from ghc/lib/std/cbits/system.c. */
-void pPrPr_disableITimers (void)
-{
-#ifdef __GLASGOW_HASKELL__
-    stopTimer();
 #endif
 }
-
-#endif

include/execvpe.h
@@ -1,27 +1,11 @@
 /* ----------------------------------------------------------------------------
    (c) The University of Glasgow 2004
 
-   Interface for code in execvpe.c
+   Interface for code in cbits/execvpe.c
    ------------------------------------------------------------------------- */
 
-#include "HsUnixConfig.h"
-// Otherwise these clash with similar definitions from other packages:
-#undef PACKAGE_BUGREPORT
-#undef PACKAGE_NAME
-#undef PACKAGE_STRING
-#undef PACKAGE_TARNAME
-#undef PACKAGE_VERSION
+extern int
+__hsunix_execvpe(const char *name, char *const argv[], char *const envp[]);
 
-#include <errno.h>
-#include <sys/types.h>
-#if HAVE_SYS_WAIT_H
-#include <sys/wait.h>
-#endif
-
-#if !defined(_MSC_VER) && !defined(__MINGW32__) && !defined(_WIN32)
-#ifndef __QNXNTO__
-extern int execvpe(char *name, char *const argv[], char **envp);
-#endif
+// implemented in cbits/ghcrts.c
 extern void pPrPr_disableITimers (void);
-#endif
-

System/Posix/Process/Internals.hs
@@ -30,7 +30,7 @@ data ProcessStatus
 foreign import ccall unsafe "pPrPr_disableITimers"
   pPrPr_disableITimers :: IO ()
 
-foreign import ccall unsafe "execvpe"
+foreign import ccall unsafe "__hsunix_execvpe"
   c_execvpe :: CString -> Ptr CString -> Ptr CString -> IO CInt
 
 decipherWaitStatus :: CInt -> IO ProcessStatus

cbits/ghcrts.c
@@ -1,0 +1,14 @@
+#include "execvpe.h"
+
+#ifdef __GLASGOW_HASKELL__
+// for 'void StopTimer(void)' prototype
+# include "Rts.h"
+#endif
+
+/* Copied verbatim from ghc/lib/std/cbits/system.c. */
+void pPrPr_disableITimers (void)
+{
+#ifdef __GLASGOW_HASKELL__
+    stopTimer();
+#endif
+}

configure.ac
@@ -39,6 +39,9 @@ AC_CHECK_FUNCS([readdir_r])
 dnl not available on android so check for it
 AC_CHECK_FUNCS([telldir seekdir])
 
+dnl This is e.g. available as a GNU extension in glibc 2.11+
+AC_CHECK_FUNCS([execvpe])
+
 AC_CHECK_MEMBERS([struct stat.st_atim])
 AC_CHECK_MEMBERS([struct stat.st_mtim])
 AC_CHECK_MEMBERS([struct stat.st_ctim])

unix.cabal
@@ -127,3 +127,4 @@ library
         cbits/HsUnix.c
         cbits/dirUtils.c
         cbits/execvpe.c
+        cbits/ghcrts.c

commit 7bad9d7d52d24cc4246646e5af3116c2dd8c25c4

Have "execvpe.h" provide execvpe() again
Turns out `process` reuses `unix`'s execvpe() implementation,
and the refactoring in f24ba78f68b2cbc4f4afadc8dd60fc2935357255
broke process.

cbits/execvpe.c
@@ -8,12 +8,13 @@
          execvpe(3) is available, we use that instead.
 
    -------------------------------------------------------------------------- */
-#include "execvpe.h"
 
-#include "HsUnixConfig.h"
+#include "execvpe.h"
 
 #if HAVE_EXECVPE
 # define _GNU_SOURCE
+#else
+# undef execvpe
 #endif
 
 #include <errno.h>

include/execvpe.h
@@ -4,8 +4,23 @@
    Interface for code in cbits/execvpe.c
    ------------------------------------------------------------------------- */
 
+#ifndef HSUNIX_EXECVPE_H
+#define HSUNIX_EXECVPE_H
+
+#include "HsUnixConfig.h"
+
 extern int
 __hsunix_execvpe(const char *name, char *const argv[], char *const envp[]);
 
+// this hack is needed for `process`; to be removed in unix-2.8
+#if HAVE_EXECVPE
+# define _GNU_SOURCE
+# include <unistd.h>
+#else
+# define execvpe(name,argv,envp) __hsunix_execvpe(name,argv,envp)
+#endif
+
 // implemented in cbits/ghcrts.c
 extern void pPrPr_disableITimers (void);
+
+#endif