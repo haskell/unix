6cebc4cb62e79e56a68bc3ad6160dff240a47efd
Bodigrim <andrew.lelechenko@gmail.com>
2022-12-08 22:37:16 +0000

    Fix tests

--- tests/Semaphore002.hs
+++ tests/Semaphore002.hs
@@ -5,7 +5,7 @@ import System.Posix
 
 main :: IO ()
 main = do
-  sem <- semOpen "/test" OpenSemFlags {semCreate = True, semExclusive = False} stdFileMode 0
+  sem <- semOpen "/test-002" OpenSemFlags {semCreate = True, semExclusive = False} stdFileMode 0
   _ <- forkIO $ do
       threadDelay (1000*1000)
       semPost sem
