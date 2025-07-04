ea507b6e0b36d95be9d9a2ff60d0c3555e7a9b71
Steven Shuck <stevenjshuck@gmail.com>
2025-07-04 00:27:17 -0400

    Explain what tests/DirEnt.c is for

--- tests/DirEnt.c
+++ tests/DirEnt.c
@@ -1,3 +1,27 @@
+/*
+ * The purpose of this C program is to do one of 4 things.  Which one it does,
+ * indicates to the DirEnt test suite something it ought to do.
+ *
+ * fail to compile
+ *   This means d_type is undefined.  It should be enough to test for
+ *   HAVE_STRUCT_DIRENT_D_TYPE, but this test suite came about because of
+ *   https://github.com/haskell/unix/issues/347 which was caused by a
+ *   misspelling of that macro, so we're testing it another way.
+ *   Absence of d_type means the test suite ought to skip testing for non-
+ *   DT_UNKNOWN values for d_type, since dirEntType returns UnknownType in this
+ *   case.
+ * exit with status 0
+ *   This means the "." entry in the DIR stream opened at ".", guaranteed to be
+ *   a directory, has a d_type of DT_DIR.  We should proceed with a test for it
+ *   in Haskell.
+ * exit with status 1
+ *   This means something unexpected went wrong.  Fail the Haskell test also.
+ * exit with stauts 2
+ *   This means the "." entry has a d_type of DT_UNKNOWN.  This is valid; no
+ *   filesystem or operating system is required to yield a useful d_type.
+ *   We should skip testing for non-DT_UNKNOWN values in Haskell.
+ */
+
 #include <dirent.h>
 #include <errno.h>
 #include <stdio.h>
